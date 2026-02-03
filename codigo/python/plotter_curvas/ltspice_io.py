8dc11128f8d6d3bece67281bca9e0c48ba51d5b3
Denise <dviltes@fi.uba.ar>
2026-02-03 01:16:42 -0300

    prueba algunas simus de spice, y agrega la opcion de leer archivos de varias variables en el plotter

--- codigo/python/plotter_curvas/ltspice_io.py
+++ codigo/python/plotter_curvas/ltspice_io.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def read_ltspice_2col(path: str, skip_header: int = 1):
+
+def _read_header(path: str) -> tuple[str, tuple[str, ...]]:
     with open(path, "r", encoding="utf-8", errors="replace") as f:
         header = ""
         for line in f:
@@ -9,12 +10,25 @@ def read_ltspice_2col(path: str, skip_header: int = 1):
                 break
 
     colnames = tuple(header.split("\t")) if "\t" in header else tuple(header.split())
+    return header, colnames
+
+
+def read_ltspice_table(path: str, skip_header: int = 1):
+    header, colnames = _read_header(path)
 
     data = np.genfromtxt(path, delimiter="\t", skip_header=skip_header)
     if data.ndim == 1 or (data.ndim == 2 and data.shape[1] < 2):
         data = np.genfromtxt(path, delimiter=None, skip_header=skip_header)
 
     if data.ndim != 2 or data.shape[1] < 2:
-        raise ValueError("No pude leer 2 columnas numéricas. Revisá separador y header.")
+        raise ValueError("No pude leer 2 o más columnas numéricas. Revisá separador y header.")
 
+    if colnames and len(colnames) != data.shape[1]:
+        colnames = tuple(f"col_{i}" for i in range(data.shape[1]))
+
+    return data, header, colnames
+
+
+def read_ltspice_2col(path: str, skip_header: int = 1):
+    data, header, colnames = read_ltspice_table(path, skip_header)
     return data[:, 0], data[:, 1], header, colnames
